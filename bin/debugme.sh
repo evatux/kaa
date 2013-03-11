24e7a90c93ea2e236b866a7c9cb61bc2a31dbec9
eva <eva@localhost>
2013-03-11 17:13:23 +0000

    [c:kaa] small script added

---
+++ bin/debugme.sh
@@ -0,0 +1,9 @@
+
+./rcm.out \
+    ../input2/stokes_3_1.csr \
+    ../output/stokes_3_1     \
+    --threshold 0.1          \
+    --graph_threshold 1      \
+    --info_file ../output/stokes_3_1.log  \
+    --png ../output/stokes_3_1
+
